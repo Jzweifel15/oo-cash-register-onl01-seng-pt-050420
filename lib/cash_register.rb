a9b8d50ac7154c55d6efc27c0d6d450a8edb25d3
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-25 18:49:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @total += (price * quantity)
+  end
+
+end
